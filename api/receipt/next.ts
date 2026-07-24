dd5da3d13ab12aeee2b4c5ef2e099a6440578a3e
Ignacio <Club_dog2@hotmail.com>
2026-07-23 23:17:32 -0300

    Vercel Fix?

---
+++ api/receipt/next.ts
@@ -0,0 +1,6 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import handler from "../_handler.js";
+
+export default async function (req: VercelRequest, res: VercelResponse) {
+  await handler(req, res, "/api/receipt/next");
+}
